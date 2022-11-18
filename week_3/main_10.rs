320e86dd6665841badc9f210c61d1197b8d06be8
os4yomore <115807276+os4yomore@users.noreply.github.com>
2022-11-18 18:45:55 +0100

    Add files via upload
    my third commit.

---
+++ week_3/main_10.rs
@@ -0,0 +1,18 @@
+fn main() {
+   // addition
+   let sum = 5550.00 + 7310.00;
+   println! ("The sum of 5550.00 and 7310.00 = {}", sum);
+
+   // subtraction
+   let difference = 95.5 - 4.3;
+   println! ("The difference of 95.5 and 4.3 = {}", difference);
+   
+   // division
+   let quotient = 56.7 / 32.2;
+   println! ("The division of 56.7 and 32.2 = {}", quotient);
+
+   // remainder
+   let remainder = 43.0 % 5.0;
+   println! ("The remainder of 43 and 5 = {}", remainder);
+
+}
